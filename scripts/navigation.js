477fd0e75dea6f7cd31e89d7b460e17bbd637776
Katy DeCorah <katydecorah@gmail.com>
2020-07-27 14:37:17 -0400

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,7 @@
 /*
  *   This content is licensed according to the W3C Software License at
  *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *   https://www.w3.org/TR/wai-aria-practices-1.1/examples/menu-button/menu-button-links.html
  */
 
 var Menubutton = function (domNode) {
